599d4695adc755de475b8bf9164c687a45592621
Keuwa <vincent.durpoix@hotmail.fr>
2018-12-31 10:53:18 +0100

    FOR WAWA

---
+++ js/wagnion.js
@@ -0,0 +1,42 @@
+answer1 = "Keuwa"
+answer2 = "la réponse d"
+answer3 = "la réponse d"
+
+url = "http://keuwa.github.io"
+
+
+function checkAnwser(){
+    page = document.title
+    answer = document.getElementById("answer").value.toLowerCase()
+    console.log(page,answer)
+
+    switch(page){
+        case "enigma_one":
+        if(answer === answer1){
+            document.location.href = url+"/wagnionlenoob";
+            return
+        }
+        error()
+        break
+        case "enigma_two":
+        if(answer === answer2){
+            document.location.href = url+"madnion";
+            return
+        }
+        error()
+        break
+        case "enigma_three":
+        if(answer === answer2){
+            document.location.href = url+"wagnionlerampant";
+            return
+        }
+        error()
+        break
+    }
+
+
+}
+
+function error(){
+    alert("Really ?")
+}
